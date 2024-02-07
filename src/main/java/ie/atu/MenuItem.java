ac1521ad5b4fcc75dd50fb0403349215bcf5531a
G00425238@atu.ie <G00425238@atu.ie>
2024-02-07 16:59:30 +0000

    Added code for all Menu Items

--- src/main/java/ie/atu/MenuItem.java
+++ src/main/java/ie/atu/MenuItem.java
@@ -2,41 +2,10 @@ package ie.atu;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class MenuItem {
-    private String name;
-    private double price;
-    private String description;
+public interface MenuItem {
 
-    public MenuItem() {
-        name = "";
-        price = 0.0;
-        description="";
+    String getName();
 
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-
+    String getDescription();
+    double getPrice();
 }
